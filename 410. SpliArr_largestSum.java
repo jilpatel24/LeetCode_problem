d9c860f8868cc9648c7cdc9588a620dc55b1a6d3
Jil patel <jilpatel1094@gmail.com>
2026-06-27 13:52:22 +0530

    Create 410. SpliArr_largestSum.java

---
+++ 410. SpliArr_largestSum.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int splitArray(int[] nums, int k) {
+         int start=0;
+        int end=0;
+
+        for(int i=0;i<nums.length;i++){
+         start= Math.max(start,nums[i]);
+         end += nums[i];
+        }
+        int mid;
+        while(start < end){
+        mid = start + (end-start)/2;
+
+        int sum = 0;
+        int pieces = 1;
+
+         for(int num : nums){
+            if(sum + num > mid){
+               //added to array
+             sum = num;
+             pieces++;
+            }else{
+               sum += num;
+            }
+         }
+         if(pieces > k){
+            start = mid+1;
+         
+         }else{
+            end = mid;
+           
+         }
+        
+    }
+  
+    return start;
+    }
+}
+    
+     
+      
+    
