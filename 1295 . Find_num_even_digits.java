95163d2b31c39acfb5c1a3aa1b1b5300c9651276
Jil patel <jilpatel1094@gmail.com>
2026-07-03 15:47:09 +0530

    Update 1295 . Find_num_even_digits.java

--- 1295 . Find_num_even_digits.java
+++ 1295 . Find_num_even_digits.java
@@ -2,7 +2,7 @@ class Solution {
     public int findNumbers(int[] nums) {
          int count = 0;
        for (int i=0;i<nums.length ;i++) {
-          int num = Math.abs(nums[i]);
+          int num = nums[i];//if array contain negative values than use abs
           int digit = 0;
           do{
             digit++;
