ffdb53cbd9b39c7a5b1c3745f1e65a6044cc5461
Jil patel <jilpatel1094@gmail.com>
2026-09-03 21:45:55 +0530

    Create 169 . Majority_Elememt.java
    Moore's Voting Algorithm

---
+++ 169 . Majority_Elememt.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+    int count = 0;
+    int ans = 0;
+    for (int i = 0; i < nums.length; i++) {
+        
+    if(count == 0){
+      ans = nums[i];
+      count=1;
+    }else if(nums[i] == ans){
+      count++;
+    }else{
+      count--;
+    }
+   }
+   //for final verify
+   count = 0;
+   for (int i = 0; i < nums.length; i++) {
+       if(nums[i] == ans){
+         count++;
+       }
+   }
+   if(count > nums.length/2){
+     return ans;
+   }
+   return -1;
+    
+    }
+}
